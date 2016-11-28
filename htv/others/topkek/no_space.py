6a96695d98ccc286e8b3e74f3a23179a899b9552
rd-li <ludanecholi@gmail.com>
2016-11-28 01:46:42 -0500

    modified for presentation

--- htv/others/topkek/no_space.py
+++ htv/others/topkek/no_space.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 with open('kek.43319559636b94db1c945834340b65d68f90b6ecbb70925f7b24f6efc5c2524e.txt', 'r') as readin:
     for line in readin:
@@ -6,10 +7,10 @@ with open('kek.43319559636b94db1c945834340b65d68f90b6ecbb70925f7b24f6efc5c2524e.
 print(output)
 morse = ''
 for each in output:
-    if each[0] == 'K' and each[1] == 'E' and each[2] == 'K':
-            morse = morse + ('-' * (len(each) - 3))
-    if each[0] == 'T' and each[1] == 'O' and each[2] == 'P':
-            morse = morse + ('.' * (len(each) - 3))
+    if each[0:3] == 'KEK':
+        morse = morse + ('-' * (len(each) - 3))
+    if each[0:3] == 'TOP':
+        morse = morse + ('.' * (len(each) - 3))
 fp = open('morsecode_no_space.txt', 'w')
 fp.write(morse)
 fp.close()
